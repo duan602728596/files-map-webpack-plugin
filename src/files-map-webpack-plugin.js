8bb4511ef50dae8b8c4085028d9dbc255ca22ad2
段昊辰 <duanhaochen@126.com>
2020-04-03 23:03:07 +0800

    refactor: 使用promise方法

--- src/files-map-webpack-plugin.js
+++ src/files-map-webpack-plugin.js
@@ -152,7 +152,9 @@ class FilesMapWebpackPlugin {
       // 判断文件夹是否存在并写入文件
       // webpack的文件输出系统
       const { outputFileSystem } = compiler;
-      const writeFilePromise = util.promisify(outputFileSystem.writeFile);
+      const writeFilePromise = outputFileSystem.promises
+        ? outputFileSystem.promises.writeFile
+        : util.promisify(outputFileSystem.writeFile);
       const mkdirPromise = createMkdirFunc(outputFileSystem);
 
       await mkdirPromise(outputDir);
